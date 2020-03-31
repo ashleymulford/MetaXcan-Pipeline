dcf37baf92256476cb3acd7c1f95f1029f3f9cec
Samuel Jaros <sjaros@luc.edu>
2020-03-31 14:12:57 -0500

    Translated SPrediXcan to bash

--- runSPrediXcan.py
+++ runSPrediXcan.py
@@ -1,17 +1,15 @@
 import os
 
-model = ""           	     # this is the path to the model transcriptome (.db file)
-covariance = ""     	     # account for related individuals <- we won't use?
-gwas = ""           	     # path to the .assoc file from the GWAS
-gwas_pattern = ""            # string representing what files to use <- might want to zip the files
+givenGWASFile = "/data/amulford/GWAS_sum_stats/YRI_GWAS_capecitabine.assoc.txt.gz"
+givenModelFolder = "/data/amulford/gtex_v8_elastic_net/eqtl_dbs/"
+
 snp_column = "rs"            # header of the column containing the SNP ids
 effect_allele = "allele1"    # header of the column containing the effect allele ???
 noneffect_allele = "allele0" # header of the column containing the non-effect allele ???
 phenotype = "beta" 	     # header of the column containing phenotype data
-pval = "p_wald"            	     # header of the column containing p-values
-output = ""          	     # path to output (.csv file)
+pval = "p_wald"            	                     # header of the column containing p-values
+output = "S-results/cap"          	     # path to output (.csv file)
 
-os.system("SPrediXcan.py --model_db_path "+model+" --covariance "+covariance+" --gwas_folder "+gwas+ \
-" --gwas_file_pattern "+gwas_pattern+" --snp_column "+snp_column+" --effect_allele_column "+effect_allele+ \
-" --non_effect_allele_column "+noneffect_allele+ " --beta_column "+phenotype+" --pvalue_column "+pval"+ \
-" --output_file "+output)
+#os.system("python3 /data/amulford/MetaXcan/software/SPrediXcan.py --model_db_path "+model+" --covariance "+covariance+" --gwas_file "+givenGWASFile+" --snp_column "+snp_column+" --effect_allele_column "+effect_allele+" --non_effect_allele_column "+noneffect_allele+ " --beta_column "+phenotype+" --pvalue_column "+pval+" --output_file "+output)
+
+os.system("bash SPrediPipe.sh -s /data/amulford/MetaXcan/software/SPrediXcan.py -m "+givenModelFolder+" --gwas "+givenGWASFile+" -o "+output)
