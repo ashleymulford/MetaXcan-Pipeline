be9ca7bf02cd9f2923c9bbf26900f953cd36077a
Samuel Jaros <sjaros@luc.edu>
2020-04-02 20:26:28 -0500

    Replacing SMetaPipe

---
+++ GWAS_pipeline.py
@@ -0,0 +1,15 @@
+import os, argparse
+
+# Needed args
+software = "~/MetaXcan/software"
+model = ""
+GWAS = ""
+snp_col = ""
+effect = ""
+noneffect = ""
+phenotype = ""
+p_val = ""
+output = ""
+other_flags = ""
+
+
