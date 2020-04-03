19b9a4a686555c6abccb9ad485c14f6a63da1956
Samuel Jaros <sjaros@luc.edu>
2020-04-02 20:27:19 -0500

    runSPrediXcan abandoned

--- runSPrediXcan.py
+++ runSPrediXcan.py
@@ -12,4 +12,4 @@ output = "S-results/cap"          	     # path to output (.csv file)
 
 #os.system("python3 /data/amulford/MetaXcan/software/SPrediXcan.py --model_db_path "+model+" --covariance "+covariance+" --gwas_file "+givenGWASFile+" --snp_column "+snp_column+" --effect_allele_column "+effect_allele+" --non_effect_allele_column "+noneffect_allele+ " --beta_column "+phenotype+" --pvalue_column "+pval+" --output_file "+output)
 
-os.system("bash SPrediPipe.sh -s /data/amulford/MetaXcan/software/SPrediXcan.py -m "+givenModelFolder+" --gwas "+givenGWASFile+" -o "+output)
+os.system("bash SPrediPipe.sh --snp rs --effect allele1 -noneffect allele0 --p_val p_wald -s /data/amulford/MetaXcan/software/SPrediXcan.py -m "+givenModelFolder+" --gwas "+givenGWASFile+" -o "+output)
