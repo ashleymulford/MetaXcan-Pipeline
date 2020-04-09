11d1948760035b9195169f842ef461ff3fb9b120
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-04-09 18:12:59 -0500

    Add files via upload

---
+++ prep_dosage_outputs.py
@@ -0,0 +1,65 @@
+#Import necessary package
+import pandas as pd
+import argparse
+from os import listdir
+from os.path import isfile, join
+
+#combine association output into single dataframe
+def combine_assoc(assoc_out_dir):
+  files = [f for f in listdir(assoc_out_dir) if isfile(join(assoc_out_dir, f))]
+  full_paths = []
+  for f in files:
+    path = assoc_out_dir+f
+    full_paths.append(path)
+  df_list = []
+  for i in range(0, len(full_paths)):
+    file = full_paths[i]
+    tiss = files[i]
+    assoc = pd.read_csv(file, delimiter = "\t")
+    #add column for tissue
+    assoc["tiss"] = tiss
+    df_list.append(assoc)
+  combo = pd.concat(df_list)
+  return combo
+    
+def get_multi(multi_out_dir):
+  files = [f for f in listdir(multi_out_dir) if isfile(join(multi_out_dir, f))]
+  for file in files:
+    path = multi_out_dir+file
+    multi = pd.read_csv(path, delimiter = "\t")
+  return multi
+  
+
+def get_sig(combo):
+  sig = combo.loc[combo['pvalue'] <= .0001]
+  return sig
+
+
+
+
+#Create flags:
+parser = argparse.ArgumentParser()
+parser.add_argument("--assoc_out_dir", required=True, help = "directory the PrediXcan association output will go into")
+parser.add_argument("--multi_out_dir", required=True, help = "directory the MulTiXcan assocation output will go into")
+parser.add_argument("--out_prefix", required=True, help = "prefix for output files")
+parser.add_argument("--pheno_prefix", required=True, help = "name of phenotype, will be added to association output file names")
+
+p = parser.parse_args()
+assoc_out_dir = p.assoc_out_dir
+multi_out_dir = p.multi_out_dir
+out_prefix = p.out_prefix
+pheno_prefix = p.pheno_prefix
+
+
+assoc_combo = combine_assoc(assoc_out_dir)
+multi = get_multi(multi_out_dir)
+
+assoc_combo.to_csv(out_prefix+"_"+pheno_prefix+"_assoc_combo.txt", sep = "\t", index = None)
+
+assoc_sig = get_sig(assoc_combo)
+multi_sig = get_sig(multi)
+
+assoc_sig.to_csv(out_prefix+"_"+pheno_prefix+"_assoc_sig.txt", sep = "\t", index = None)
+multi_sig.to_csv(out_prefix+"_"+pheno_prefix+"_multi_sig.txt", sep = "\t", index = None)
+
+
