dc4c044be1f1fd1fb09a0533aecd217717df9e30
Samuel Jaros <sjaros@luc.edu>
2020-04-09 00:31:13 -0500

    Master script updates

--- GWAS_pipeline.py
+++ GWAS_pipeline.py
@@ -3,8 +3,8 @@ import os, argparse
 # Parse args
 p = argparse.ArgumentParser()
 p.add_argument("-d","--db_dir", required=True, help="directory for the model to predict against (both *.db and *.txt.gz files")
-p.add_argument("-s","--scripts_dir", default="~/MetaXcan/Software", help="directory containing the MetaXcan scripts, called 'software' in the MetaXcan package")
-p.add_argument("-g","--gwas_path", required=True, help="file containing the GWAS summary statistics (*.assoc.txt.gz)")
+p.add_argument("-s","--scripts_dir", default="~/MetaXcan/software", help="directory containing the MetaXcan scripts, called 'software' in the MetaXcan package")
+p.add_argument("-g","--gwas_file", required=True, help="file containing the GWAS summary statistics (*.assoc.txt.gz)")
 p.add_argument("-o","--output", default="~/out", help="directory for output files (*.csv and SMultiXcan.txt")
 p.add_argument("--snp_col", default="variant_id", help="name of the column containing SNP data in the GWAS summary statistics")
 p.add_argument("--effect","--effect_col", default="effect_allele", help="name of the column containing the effect allele data in the GWAS summary statistics")
@@ -13,12 +13,13 @@ p.add_argument("--phenotype", "--beta", default="beta", help="name of the column
 p.add_argument("-p","--p_val", default="p_value", help="name of the column containing the p-value in the GWAS summary statistics")
 p.add_argument("--snp_cov", required=True, help="file containing SNP covariance which comes in the model download (*_covariance.txt)")
 p.add_argument("--cutoff_threshold", default=0.4, help="threshold of variance eigenvalues when truncating SVD")
+p.add_argument("--out_prefix", default='', help="text to add to the beginning of every file name produced by this run")
 
 a = p.parse_args()
 # Needed args
 software = a.scripts_dir
 model = a.db_dir
-GWAS = a.gwas_path
+GWAS = a.gwas_file
 snp_col = a.snp_col
 effect = a.effect
 noneffect = a.noneffect
@@ -27,6 +28,7 @@ p_val = a.p_val
 output = a.output
 snp_cov = a.snp_cov
 cutoff = a.cutoff_threshold
+out_prefix = a.out_prefix
 
 # TODO check paths & files
 
@@ -44,10 +46,10 @@ for file in os.listdir(model):
 		tissue = file[:-3]
 		os.system("python3 "+software+"/SPrediXcan.py --model_db_path "+model+"/"+tissue+".db --covariance "+model+"/"+tissue+".txt.gz"+
 			  " --gwas_file "+GWAS+" --snp_column "+snp_col+" --effect_allele_column "+effect+" --non_effect_allele_column "+noneffect+
-			  " --beta_column "+phenotype+" --pvalue_column "+p_val+" --output_file "+output+"/"+tissue+"_predict.csv --throw")
+			  " --beta_column "+phenotype+" --pvalue_column "+p_val+" --output_file "+output+"/"out_prefix+tissue+"_predict.csv --throw")
 
 # Call SMultiXcan
 os.system("python3 "+software+"/SMulTiXcan.py --models_folder "+model+" --models_name_pattern '(.*).db' --snp_covariance "+snp_cov+" --gwas_file "+GWAS+" --snp_column "+snp_col+
 " --effect_allele_column "+effect+" --non_effect_allele_column "+noneffect+" --beta_column "+phenotype+" --pvalue_column "+p_val+
-" --metaxcan_folder "+output+" --metaxcan_filter '(.*)_predict.csv' --metaxcan_file_name_parse_pattern '()(.*)_predict.csv' --cutoff_threshold "+str(cutoff)+
+" --metaxcan_folder "+output+" --metaxcan_filter '"+out_prefix+"(.*)_predict.csv' --metaxcan_file_name_parse_pattern '"+out_prefix+"()(.*)_predict.csv' --cutoff_threshold "+str(cutoff)+
 " --output "+output+"/SMulTiXcan.txt --throw")
--- MetaXcan_Pipeline.py
+++ MetaXcan_Pipeline.py
@@ -31,7 +31,7 @@ dos.add_argument("--multi_out_dir", help="Output for the MultiXcan association, 
 
 # Arguments for GWAS
 gwas = p.add_argument_group(title="GWAS",description="Flags for running MetaXcan based on GWAS summary statistics")
-gwas.add_argument("-g", "--gwas_path", help="File containing the GWAS summary statistics")
+gwas.add_argument("-g", "--gwas_file", help="File containing the GWAS summary statistics")
 gwas.add_argument("--snp_cov", help="File containing the SNP covariance. This normally comes with the model download and usually ends with _covariance.txt)")
 cutoff = gwas.add_mutually_exclusive_group()   # TODO Which does Dr. Wheeler use?
 cutoff.add_argument("--cutoff_condition_number", help="Condition number of eigen values to use when truncated SVD components")
@@ -41,14 +41,77 @@ cutoff.add_argument("--cutoff_trace_ratio", help="Ratio of eigenvalues to trace,
 gwas.add_argument("--snp_col", default="variant_id", help="Name of the column in the GWAS summary statistics with the SNP data")
 gwas.add_argument("--effect_col", default="effect_allele", help="Name of the column in the GWAS summary statistics with the effect allele")
 gwas.add_argument("--noneffect_col", default="noneffect_allele", help="Name of the column in the GWAS summary statistics with the noneffect allele")
-gwas.add_argument("--phenotype", "--beta", default="beta", help="Name of the column in the GWAS summary statistics with the phenotype data")
-gwas.add_argument("-p", "--p_val", default="p_value", help="Name of the column in the GWAS summary statistics with the p-values")
+gwas.add_argument("--phenotype_col", "--beta_col", default="beta", help="Name of the column in the GWAS summary statistics with the phenotype data")
+gwas.add_argument("-p", "--p_val_col", default="p_value", help="Name of the column in the GWAS summary statistics with the p-values")
 
 arg=p.parse_args()
 ### Test for valid input
-if arg.force_dosage and arg.force_gwas:
-	raise ValueError("Use of both --force_dosage and --force_gwas is illegal")
 # Warn if -software doesn't end with software
+# Raise exception if not GWAS summary stat
+# Raise exception if not dosage format
 
-def run_dosage():
-	os.system("dosage_pipeline.py ")
+### Methods for running next script
+# Gets input ready and runs dosage_pipeline.py
+def runDosage():
+	pred_out_dir = arg.pred_out_dir if arg.pred_out_dir else arg.output
+	assoc_out_dir = arg.assoc_out_dir if arg.assoc_out_dir else arg.output
+	multi_out_dir = arg.multi_out_dir if arg.multi_out_dir else arg.output
+	model_type = " --mashr" if arg.mashr else " --mesa" if arg.mesa else ""
+	
+	os.system("dosage_pipeline.py --db_dir "+arg.db_dir+" --scripts_dir "+arg.software+" --geno_dir "+arg.geno_dir+
+            " --geno_file_pattern '"+arg.geno_file_pattern+"' --sample_path "+arg.sample_path+" --out_prefix "+arg.out_prefix+
+            " --pheno_path "+arg.pheno_path+" --pheno_col "+arg.pheno_col+" --pheno_prefix "+arg.pheno_col+" --pred_out_dir "+pred_out_dir+
+            " --assoc_out_dir "+assoc_out_dir+" --multi_out_dir "+multi_out_dir+model_type)
+
+# Gets input ready and runs GWAS_pipeline.py
+def runGWAS():
+	cutoff = "--cutoff_condition_number "+str(arg.cutoff_condition_number) if arg.cutoff_condition_number else \
+		 "--cutoff_eigen_ratio "+str(arg.cutoff_eigen_ratio) if arg.cutoff_eigen_ratio else \
+		 "--cutoff_threshold "+str(arg.cutoff_threshold) if arg.cutoff_threshold else \
+		 "--cutoff_trace_ratio "+str(arg.cutoff_trace_ratio) if arg.cutoff_trace_ratio else \
+		 ""
+	if cutoff == "":
+		raise TypeError("No cutoff defined. Use --cutoff_...")
+	
+	os.system("dosage_pipeline.py --db_dir "+arg.db_dir+" --scripts_dir "+arg.software+" -g "+arg.gwas_file+cutoff+
+            " --snp_cov "+arg.snp_cov+" --snp_col "+arg.snp_col+" --effect "+arg.effect_col+" --noneffect "+arg.noneffect_col+
+            " --beta "+arg.beta_col+" -p "+arg.p+" -o "+arg.o+" --out_prefix "+arg.out_prefix)
+
+### Which one is right?
+
+if arg.force_dosage or (arg.geno_dir and arg.geno_file_pattern and arg.sample_path and arg.pheno_path and arg.pheno_col):
+	if arg.g:
+		warnings.warn("GWAS file (-g, --gwas_file) specified, but will not be used")
+	if arg.snp_cov:
+		warnings.warn("SNP covariance file (--snp_cov) speficied, but will not be used")
+	if arg.cutoff_condition_number or arg.cutoff_eigen_ratio or arg.cutoff_threshold or arg.cutoff_trace_ratio:
+		warnings.warn("Cutoff (--cutoff_...) speficied, but will not be used")
+	if arg.snp_col:
+		warnings.warn("SNPs column name (--snp_col) specified, but will not be used")
+	if arg.effect_col:
+		warnings.warn("Effect allele column name (--effect_col) specified, but will not be used")
+	if arg.noneffect_col:
+		warnings.warn("Non-effect allele column name (--noneffect_col) specified, but will not be used")
+	if arg.beta_col:
+		warnings.warn("Phenotype column name (--phenotype_col, --beta_col) specified, but will not be used")
+	if arg.p:
+		warnings.warn("P-value column name (-p, --p_val_col) specified, but will not be used")
+	
+	runDosage()
+	
+elif arg.force_gwas or (arg.g and arg.snp_cov and (arg.cutoff_condition_number or arg.cutoff_eigen_ratio or arg.cutoff_threshold or arg.cutoff_trace_ratio)):
+	if arg.geno_dir:
+		warnings.warn("Genotypes file (--geno_dir) specified, but will not be used")
+	if arg.geno_file_pattern:
+		warnings.warn("Genotypes file pattern (--geno_file_pattern) specified, but will not be used")
+	if arg.sample_path:
+		warnings.warn("Samples file (--sample_path) specified, but will not be used")
+	if arg.pheno_path:
+		warnings.warn("Phenotype file (--pheno_path) specified, but will not be used")
+	if arg.pheno_col:
+		warnings.warn("Phenotype column name (--pheno_col) specified, but will not be used")
+	if arg.mashr or arg.mesa:
+		warnings.warn("Model type (--mashr, --mesa) specified, but will not be used")
+	# TODO warn about other output options or add other options to GWAS_pipeline.py
+	
+	runGWAS()
