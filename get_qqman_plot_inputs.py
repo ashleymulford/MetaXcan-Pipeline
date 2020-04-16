8ca30c168963cf9bebf5c6d42213a2ab8d98bf22
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-04-16 17:15:01 -0500

    Add files via upload

---
+++ get_qqman_plot_inputs.py
@@ -0,0 +1,24 @@
+import argparse
+
+#Create flags:
+parser = argparse.ArgumentParser()
+parser.add_argument("--assoc_out_dir", required=True, help = "directory the PrediXcan Association output will go into")
+parser.add_argument("--multi_out_dir", required=True, help = "directory the MulTiXcan output will go into")
+parser.add_argument("--out_prefix", required=True, help = "prefix for output files")
+parser.add_argument("--pheno_prefix", default = "", help = "Optional, name of phenotype")
+
+
+#Parse arguments inputted by user:
+p = parser.parse_args()
+assoc_out_dir = p.assoc_out_dir
+multi_out_dir = p.multi_out_dir
+out_prefix = p.out_prefix
+pheno_prefix = p.pheno_prefix
+
+
+file = open("input_file_names_qqman.txt", "w")
+
+file.write(assoc_out_dir + "\n")
+file.write(multi_out_dir + "\n")
+file.write(out_prefix + "\n")
+file.write(pheno_prefix)
---
+++ qqman_plots.R
@@ -0,0 +1,69 @@
+#Import necessary libraries
+library(data.table)
+library(dplyr)
+library(qqman)
+library(colorspace)
+
+#function to paste
+"%&%" = function(a,b) paste(a,b,sep="")
+
+#Create color vector
+colors<-qualitative_hcl(3,"Dark3")
+
+input_files<-fread("input_file_names_qqman.txt", header = FALSE)
+
+assoc_dir<-input_files$V1[1]
+multi_dir<-input_files$V1[2]
+out_prefix<-input_files$V1[3]
+if (exists(input_files$V1[4])){
+  pheno_prefix<-input_files$V1[4]
+}
+
+
+sig_files<-fread("sig_file_names.txt", header = FALSE)
+
+fnames<-sig_files$V1
+
+chrom<-fread("/data/amulford/chrom_anno_gtexv8.txt")
+
+for (fname in fnames){
+  sig_hits <- fread(fname)
+  
+  if("tiss" %in% names(sig_hits)) {
+    tiss<-sig_hits$tiss[1]
+    assoc_model_path<-assoc_dir %&% tiss
+    model<-fread(assoc_model_path)
+  } else {
+    multi_name<-sub("_sig", "", fname)
+    multi_model_path<-multi_dir %&% multi_name
+    model<-fread(multi_model_path)
+  }
+
+  if("gene_name" %in% names(sig_hits)) {
+    model_chrom<- left_join(model, chrom, by = c("gene" = "ensg_ids", "gene_name" = "gene_name"))
+  } else {
+    model_chrom<-left_join(model, chrom, by = c("gene" = "ensg_ids"))
+  }
+  
+  if("status" %in% names(model_chrom)) {
+    model_chrom<-select(model_chrom, - "status")
+  }
+
+  model_chrom_small<-model_chrom[complete.cases(model_chrom), ]
+  
+  fname_small<-sub("_sig.txt", "", fname)
+  
+  png(filename = fname_small %&% ".qqplot.png", res=100)
+  qq(model_chrom$pvalue)
+  dev.off()
+
+  if(dim(model_chrom_small)[1] != 0) {
+    png(filename = fname_small %&% ".manplot.png", res=100)
+    manhattan(model_chrom_small, chr = "chr", bp = "pos", p = "pvalue", col = colors)
+    dev.off()
+  }
+  
+}
+  
+
+
