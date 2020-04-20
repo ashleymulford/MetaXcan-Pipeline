eec6d5d1c053a79d5a3d42a4f37c677519e77ba2
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-04-20 16:42:39 -0500

    Update get_qqman_plot_inputs.py

--- get_qqman_plot_inputs.py
+++ get_qqman_plot_inputs.py
@@ -6,6 +6,7 @@ parser.add_argument("--assoc_out_dir", required=True, help = "directory the Pred
 parser.add_argument("--multi_out_dir", required=True, help = "directory the MulTiXcan output will go into")
 parser.add_argument("--out_prefix", required=True, help = "prefix for output files")
 parser.add_argument("--pheno_prefix", default = "", help = "Optional, name of phenotype")
+parser.add_argument("--chrom_anno_path", required=True, help = "path to bp chromosome annotation file")
 
 
 #Parse arguments inputted by user:
@@ -14,6 +15,7 @@ assoc_out_dir = p.assoc_out_dir
 multi_out_dir = p.multi_out_dir
 out_prefix = p.out_prefix
 pheno_prefix = p.pheno_prefix
+chrom_anno_path = p.chrom_anno_path
 
 
 file = open("input_file_names_qqman.txt", "w")
@@ -21,4 +23,5 @@ file = open("input_file_names_qqman.txt", "w")
 file.write(assoc_out_dir + "\n")
 file.write(multi_out_dir + "\n")
 file.write(out_prefix + "\n")
+file.write(chrom_anno_path + "\n")
 file.write(pheno_prefix)
