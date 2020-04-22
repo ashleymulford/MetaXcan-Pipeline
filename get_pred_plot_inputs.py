a04dfb3e3572615e72c23bebc9588c77d591ea52
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-04-21 20:01:58 -0500

    Update get_pred_plot_inputs.py

--- get_pred_plot_inputs.py
+++ get_pred_plot_inputs.py
@@ -7,8 +7,8 @@ parser.add_argument("--pred_out_dir", required=True, help = "directory the predi
 parser.add_argument("--out_prefix", required=True, help = "prefix for output files")
 parser.add_argument("--pheno_col", required=True, help = "name of column in phenotype file with phenotype data")
 parser.add_argument("--pheno_prefix", required=True, help = "name of phenotype, will be added to association output file names")
-
-
+parser.add_argument("--mashr", action="store_true", help = "specify if mashr models were used")
+parser.add_argument("--mesa", action="store_true", help = "specify if mesa models were used")
 
 
 #Parse arguments inputted by user:
@@ -18,6 +18,15 @@ pred_out_dir = p.pred_out_dir
 out_prefix = p.out_prefix
 pheno_col = p.pheno_col
 pheno_prefix = p.pheno_prefix
+if p.mashr:
+  model_type = "mashr"
+elif p.mesa:
+  model_type = "mesa"
+else:
+  model_type = "en"
+
+
+print("running get_pred_inputs")
 
 
 file = open("input_file_names_pred.txt", "w")
@@ -26,4 +35,5 @@ file.write(pheno_path + "\n")
 file.write(pred_out_dir + "\n")
 file.write(out_prefix + "\n")
 file.write(pheno_col + "\n")
+file.write(model_type + "\n")
 file.write(pheno_prefix)
